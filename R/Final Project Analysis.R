7dde44f69549e9f88f09fb475c4e8587c84e1ef2
Aditya Mehta <aditmehta2001@gmail.com>
2024-08-27 17:06:38 -0400

    Pushing raw data (CSV file) and R script with analysis to GitHub Final Project repo.

---
+++ R/Final Project Analysis.R
@@ -0,0 +1,3 @@
+#Loading polling place data from GitHub TidyTuesday repo
+polling_places <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-01-16/polling_places.csv')
+
